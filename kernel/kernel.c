49fc26906c96669f4ea17e3ffb280bf104dfc5a4
anton2920 <anton2920@gmail.com>
2022-07-29 17:50:02 +0300

    Changed bootloader and kernel to use new utils

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,26 +1,8 @@
-enum vga_colors {
-    VGA_WHITE = 0xF
-};
+#include "../drivers/screen.h"
 
 
-void print_vga(const char *text, char start_row, char start_col, char color)
+void kernel_main(void)
 {
-    unsigned short *vga_text_buffer = (unsigned short *) ((unsigned char *) 0xB8000 + 2 * (start_row * 80 + start_col));
-
-    while (*text != '\0') {
-        *vga_text_buffer = (color << 8) | (*text);
-        ++vga_text_buffer;
-
-        ++text;
-    }
-}
-
-
-#define BASE_LINE 1
-
-
-void kernel_main()
-{
-    print_vga("done. Now in Intel's protected mode.", BASE_LINE, 11, VGA_WHITE);
-    print_vga("Loaded ProOS alpha 0.1", BASE_LINE + 1, 0, VGA_WHITE);
+    k_screen_prints("done.\r\n"); /* "Booting... */
+    k_screen_prints("Loaded ProOS alpha 0.1");
 }
