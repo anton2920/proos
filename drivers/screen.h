f4b35bdb250d82fb2d6feacbbea70110e2b01909
anton2920 <anton2920@gmail.com>
2022-07-29 18:19:06 +0300

    Added function for printing single characters; added more VGA colors

--- drivers/screen.h
+++ drivers/screen.h
@@ -1,8 +1,11 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
+#define VGA_TEXT_BG(x)    (x << 4)
+#define VGA_TEXT_FG(x)    (x)
 
-enum vga_colors {
+
+enum vga_palette_colors {
     VGA_BLACK = 0x0,
     VGA_BLUE,
     VGA_GREEN,
@@ -18,12 +21,17 @@ enum vga_colors {
     VGA_LIGHT_RED,
     VGA_LIGHT_MAGENTA,
     VGA_YELLOW,
-    VGA_WHITE
+    VGA_WHITE,
+    VGA_NCOLORS
 };
 
 
+void k_screen_printc_ex(char ch, unsigned char style);
+void k_screen_printc(char ch);
+
 void k_screen_prints_ex(const char *str, unsigned char attr);
 void k_screen_prints(const char *str);
+
 void k_screen_clear(void);
 
 #endif // SCREEN_H
