62f147daedc1897cc058ffdb3ed2d7a682d98ecf
anton2920 <anton2920@gmail.com>
2022-08-01 15:42:29 +0300

    Added beep as a part of 'k_intro()'

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,7 +6,7 @@ static void k_intro(void)
     /* Finishing "Booting... " message */
     k_screen_prints("done.\r\n");
 
-    k_screen_prints_ex("Loaded ProOS " VERSION "\r\n", VGA_TEXT_FG(VGA_LIGHT_CYAN));
+    k_screen_prints_ex("Loaded ProOS " VERSION "\r\n\a", VGA_TEXT_FG(VGA_LIGHT_CYAN));
 }
 
 
