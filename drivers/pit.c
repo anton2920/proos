77c81a273e503f04da6476e988d08f21a88cbec1
anton2920 <anton2920@gmail.com>
2022-08-02 23:02:54 +0300

    Changed PIT mode from 'square wave' to 'rate'

--- drivers/pit.c
+++ drivers/pit.c
@@ -14,7 +14,7 @@ void k_pit_timer_phase(int hz)
 
     divisor = 1193180 / hz;       /* Calculate our divisor */
 
-    outb(0x43, 0x36);             /* Set our command byte 0x36 */
+    outb(0x43, 0x34);             /* Set our command byte 0x36 */
     outb(0x40, divisor & 0xFF);   /* Set low byte of divisor */
     outb(0x40, divisor >> 8);     /* Set high byte of divisor */
 }
