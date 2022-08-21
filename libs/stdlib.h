b02fe1def59f0acf54bc557c7460a6d216a78d37
anton2920 <anton2920@gmail.com>
2022-08-21 15:42:15 +0300

    Removed 'mem*()' declarations from 'stdlib.h'

--- libs/stdlib.h
+++ libs/stdlib.h
@@ -4,8 +4,4 @@
 extern void halt(void);
 void abort(void);
 
-void *memset(void *dest, int c, unsigned long n);
-void *memcpy(void *_dest, const void *_src, unsigned long size);
-int memcmp(const void *s1, const void *s2, unsigned long n);
-
 #endif // STDLIB_H
