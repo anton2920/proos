56c8b2a88cf1528d435256c0cf3ad73b3e813353
anton2920 <anton2920@gmail.com>
2022-08-21 17:09:05 +0300

    Moved and enhanced 'stdio.h' header

--- libs/stdio.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOW_LEVEL_H
-#define LOW_LEVEL_H
-
-unsigned char inb(unsigned short port);;
-void outb(unsigned short port, unsigned char data);
-unsigned short inw(unsigned short port);
-void outw(unsigned short port, unsigned short data);
-
-#endif // LOW_LEVEL_H
